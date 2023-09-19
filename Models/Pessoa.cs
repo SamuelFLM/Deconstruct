0173c1c7af84fce56b3be46bba93369b11dd6073
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-09-19 17:37:16 -0300

    att

---
+++ Models/Pessoa.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Destruct.Models
+{
+    public abstract class Pessoa
+    {
+        public Pessoa() { }
+        public Pessoa(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+        protected string Nome { get; set; }
+        protected int Idade { get; set; }
+
+        public abstract void Apresentar();
+    }
+}
