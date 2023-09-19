0871e165bfe270f13780a96a05f8336c64c95653
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-09-19 17:52:56 -0300

    deconstruct

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@ using Destruct.Models;
 
 Fisica pf = new Fisica("Samuel", 21, "124.223.092-01");
 
-pf.Apresentar();
+// pf.Apresentar();
 
 
 var (nome, idade, cpf) = pf;
+
+Console.WriteLine(nome);
+Console.WriteLine(cpf);
+Console.WriteLine(idade);
